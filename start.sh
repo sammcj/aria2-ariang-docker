c8b1bf903b619cb6bab21e4eca22c7ddfe3a9b27
Sam <sammcj@users.noreply.github.com>
2025-05-03 09:27:59 +1000

    minor updates

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ fi
 touch $conf_path/aria2.session
 
 if [ -n "$ARIA2RPCPORT" ]; then
-    echo "Changing RPC request port to $ARIA2RPCPORT"
+    echo "Changing RPC request port to $ARIA2RPCPORT in AriaNg WebUI"
     sed -i "s/6800/${ARIA2RPCPORT}/g" $ariang_js_path
 fi
 
@@ -42,10 +42,36 @@ fi
 chown -R "$userid":"$groupid" $conf_path
 chown -R "$userid":"$groupid" $data_path
 
-# Serve AriaNg static files using a lightweight web server (busybox httpd)
-echo "Starting lightweight web server for AriaNg on port 8080"
-busybox httpd -f -p 8080 -h /usr/local/www/ariang &
+# Make sure the RPC port is correctly set in aria2.conf
+if ! grep -q "^rpc-listen-port=" $conf_path/aria2.conf; then
+    # If rpc-listen-port is not in config, add it
+    echo "Adding rpc-listen-port=6800 to aria2.conf"
+    echo "rpc-listen-port=6800" >> $conf_path/aria2.conf
+elif [ -n "$ARIA2_RPC_PORT" ]; then
+    # If ARIA2_RPC_PORT is set, update the config
+    echo "Setting rpc-listen-port to $ARIA2_RPC_PORT in aria2.conf"
+    sed -i "s/^rpc-listen-port=.*/rpc-listen-port=${ARIA2_RPC_PORT}/" $conf_path/aria2.conf
+fi
 
-# Start aria2c in foreground
+# Ensure rpc-listen-all is enabled
+if ! grep -q "^rpc-listen-all=true" $conf_path/aria2.conf; then
+    echo "Ensuring rpc-listen-all=true in aria2.conf"
+    sed -i '/^rpc-listen-all=/d' $conf_path/aria2.conf
+    echo "rpc-listen-all=true" >> $conf_path/aria2.conf
+fi
+
+# Debug info
+echo "Current network configuration:"
+ip addr
+echo "Current listening ports:"
+netstat -ltn || echo "netstat not available"
+
+# Run busybox httpd with more debug info
+echo "Starting lightweight web server for AriaNg on port 8080"
+busybox httpd -fvv -p 8080 -h /usr/local/www/ariang &
+HTTPD_PID=$!
+echo "Busybox httpd started with PID $HTTPD_PID"
+
+# Start aria2c in foreground with some basic logging
 echo "Starting aria2c RPC server"
-su-exec "$userid":"$groupid" aria2c "$@"
+exec su-exec "$userid":"$groupid" aria2c --conf-path="$conf_path/aria2.conf" "$@"
